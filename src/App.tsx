056d49ff4d67be05336dbbfd043a1dc43f7ecd31
oleksiivelychko <alexoleksiivelychko@gmail.com>
2021-05-08 16:06:48 +0300

    create login page

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,17 @@ import {
   Route, Link
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import logo from './styles/app/logo.svg';
-import './styles/app/app.css';
+import LoginPage from "./pages/auth/LoginPage";
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    <Link to="/">Home</Link>
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                </a>
-            </header>
-        </div>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
         <Switch>
           <Route path="/" component={HomePage} exact />
+          <Route path="/login" component={LoginPage} />
         </Switch>
       </BrowserRouter>
     )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import logo from "../styles/app/logo.svg";
+import './../styles/app/app.css';
 
 export default class HomePage extends React.Component {
   render() {
     return (
-      <div className="content">
-        <h1>Home page</h1>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Home page</h1>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+          </a>
+        </header>
       </div>
     );
   }

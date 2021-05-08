135be95f9c1aedf85945827702a9db7424d458c2
oleksiivelychko <alexoleksiivelychko@gmail.com>
2021-05-08 14:35:14 +0300

    create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route, Link
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import logo from './styles/app/logo.svg';
+import './styles/app/app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    <Link to="/">Home</Link>
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                </a>
+            </header>
+        </div>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="content">
+        <h1>Home page</h1>
+      </div>
+    );
+  }
+}

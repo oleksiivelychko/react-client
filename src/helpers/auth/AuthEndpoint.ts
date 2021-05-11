9ccd820ae307fbb6c4c3dad270ede62cdf1b93ea
oleksiivelychko <alexoleksiivelychko@gmail.com>
2021-05-11 20:31:40 +0300

    load .env - add react_app prefix

--- src/helpers/auth/AuthEndpoint.ts
+++ src/helpers/auth/AuthEndpoint.ts
@@ -1,8 +1,8 @@
 export default function authEndpoint() {
-    const API_AUTH_URL = process.env.SERVER_URL;
+    const API_AUTH_URL = process.env.REACT_APP_SERVER_URL;
 
     if (API_AUTH_URL) {
-        return `${API_AUTH_URL}/api/auth`
+        return `${API_AUTH_URL}/api/auth`;
     } else {
         throw URIError('Did not managed to get SERVER_URL from .env');
     }

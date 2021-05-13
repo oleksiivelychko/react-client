25d04e7feeeb9980aa15c8e76f8e816a03e471e3
oleksiivelychko <alexoleksiivelychko@gmail.com>
2021-05-13 06:59:59 +0300

    wrap register page into reducer

--- src/components/pages/auth/RegisterPage.tsx
+++ src/components/pages/auth/RegisterPage.tsx
@@ -1,20 +1,112 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import {Link, Redirect} from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { validatePassword, validateEmail, validateUsername, confirmPassword } from "../../../validators/BaseSchema";
+import { connect } from "react-redux";
+import { register } from "../../../actions/auth/AuthActions";
+import "../../../styles/auth.scss";
 
-export default class RegisterPage extends React.Component {
-  render(){
-    return (
-      <div className="auth">
-        <form className="auth__form auth__form-signup">
-          <h1 className="h3 mb-3 font-weight-normal">Sign up</h1>
-          <input type="email" id="inputName" className="form-control" placeholder="Name" name="name" required />
-          <input type="email" id="inputEmail" className="form-control" placeholder="Email" name="email" required />
-          <input type="password" id="inputPassword" className="form-control" placeholder="Password" name="password" required />
-          <input type="password" id="inputPasswordConfirm" className="form-control" placeholder="Password confirm" name="password_confirm" required />
-          <button className="btn btn-lg btn-primary btn-block" type="submit">Register</button>
-          <p className="mt-4 mb-3 text-muted"><Link to='/'>Go home</Link></p>
-        </form>
-      </div>
-    );
-  }
+class RegisterPage extends React.Component<any, any> {
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+          loading: false,
+          successful: false,
+        };
+    }
+
+    handleSubmit(values: { username: string, email: string; password: string }): boolean {
+        this.setState({
+            loading: true,
+            successful: false,
+        });
+
+        this.props.dispatch(register(values.username, values.email, values.password))
+            .then(() => {
+                this.setState({
+                    loading: false,
+                    successful: true,
+                });
+            })
+            .catch(() => {
+                this.setState({
+                    loading: false,
+                    successful: false,
+                });
+            });
+
+        return false;
+    }
+
+    render() {
+        const { isLoggedIn, message } = this.props;
+        if (isLoggedIn) {
+            return <Redirect to='/profile' />;
+        }
+
+        return (
+            <div className="auth">
+                <Formik
+                    initialValues={{ username: '', email: '', password: '', password_confirm: '' }}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setSubmitting(this.handleSubmit(values));
+                    }}
+                >
+                    {({ values, errors, isSubmitting}) => (
+                        <Form className="auth__form auth__form-signup">
+                            <h1 className="h3 mb-3 font-weight-normal">Sign up</h1>
+                            <Field
+                                type="text"
+                                name="username"
+                                className={'form-control mt-1 ' + (errors.username ? 'alert alert-danger' : '')}
+                                placeholder="Username"
+                                validate={validateUsername}
+                            />
+                            <Field
+                                type="email"
+                                name="email"
+                                className={'form-control mt-1 ' + (errors.email ? 'alert alert-danger' : '')}
+                                placeholder="Email"
+                                validate={validateEmail}
+                            />
+                            <ErrorMessage className="text-danger" name="email" component="div" />
+                            <Field
+                                type="password"
+                                name="password"
+                                className={'form-control mt-1 ' + (errors.password ? 'alert alert-danger' : '')}
+                                placeholder="Password"
+                                validate={validatePassword}
+                            />
+                            <ErrorMessage className="text-danger" name="password" component="div" />
+                            <Field
+                                type="password"
+                                name="password_confirm"
+                                className={'form-control mt-1 ' + (errors.password_confirm ? 'alert alert-danger' : '')}
+                                placeholder="Confirm password"
+                                validate={(value: string) => confirmPassword(values.password, value)}
+                            />
+                            <ErrorMessage className="text-danger" name="password_confirm" component="div" />
+                            <button type="submit" disabled={isSubmitting} className="btn btn-lg btn-primary btn-block mt-3">
+                                {this.state.loading && (<span className="spinner-border spinner-border-sm" />)}
+                                Register
+                            </button>
+
+                            {message && (<div className={'mt-4 alert ' + (this.state.successful ? 'alert-success' : 'alert-danger') } role="alert">{message}</div>)}
+                            <p className="mt-4 text-muted"><Link to='/'>Go Home</Link></p>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
+        );
+    }
 }
+
+function mapStateToProps(state: { message: { message: any; }; }) {
+    const { message } = state.message;
+    return {
+        message,
+    };
+}
+
+export default connect(mapStateToProps)(RegisterPage);

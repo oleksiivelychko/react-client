660bd25d0628afff0e8b3d0a7dcaa69c24ffa3ce
oleksiivelychko <alexoleksiivelychko@gmail.com>
2021-05-13 07:04:40 +0300

    reformat page

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../styles/app/logo.svg";
-import './../../styles/app/app.css';
-import {Link} from 'react-router-dom';
+import "./../../styles/app/app.css";
+
 
 export default class HomePage extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Home page</h1>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-          </a>
-          <Link to='/login'>Login</Link>
-          <Link to='/register'>Register</Link>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='App'>
+                <header className='App-header'>
+                    <img src={logo} className='App-logo' alt='logo' />
+                    <h1>Home page</h1>
+                    <a
+                        className='App-link'
+                        href='/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                    </a>
+                    <Link to='/login'>Login</Link>
+                    <Link to='/register'>Register</Link>
+                </header>
+            </div>
+        );
+    }
 }

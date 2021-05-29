7542448cbfc00a9c4e6a9daa44b337e5a8c07783
oleksiivelychko <alexoleksiivelychko@gmail.com>
2021-05-29 13:14:44 +0300

    remove detailed error log

--- src/services/auth/AuthService.ts
+++ src/services/auth/AuthService.ts
@@ -14,17 +14,7 @@ class AuthService {
             })
             .catch(function (error) {
                 if (error.response) {
-                    // Request made and server responded
-                    //console.log(error.response.data);
-                    //console.log(error.response.status);
-                    //console.log(error.response.headers);
                     return error.response;
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    //console.log(error.request);
-                } else {
-                    // Something happened in setting up the request that triggered an Error
-                    //console.log('Error', error.message);
                 }
                 throw Error(error.toString())
             });
